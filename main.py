43dca15773914c7067ab9ab5317dd70b0c1e0e76
zanniboni <viny.zanchagas@gmail.com>
2021-09-21 12:38:43 -0300

    Update main.py
    Adicionado exemplo de load do modelo

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # ----------- Código para executar o modelo treinado 'nota_50'
 # Specify the path to your image
 from detecto import core, utils, visualize
-model = core.Model(['nota_50.pth'])
-image = utils.read_image('frames/dinheiro2.jpg')
-predictions = model.predict(image)
+
+
+model = core.Model.load('nota_50.pth', ['nota_50'])
+# model = core.Model(['nota_50'])
+image = utils.read_image('frames/dinheiro13.jpg')
+predictions = model.predict_top(image)
+# predictions = model.predict(image)
 # predictions format: (labels, boxes, scores)
 labels, boxes, scores = predictions
 # ['alien', 'bat', 'bat']

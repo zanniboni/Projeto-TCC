ebc3803a7ea465989e5d5bddc96479fea0f8322e
zanniboni <viny.zanchagas@gmail.com>
2021-10-30 12:16:30 -0300

    Update camera_detection.py

--- camera_detection.py
+++ camera_detection.py
@@ -3,8 +3,10 @@
 from detecto import core, utils, visualize
 import cv2
 import pyttsx3
+import numpy as np
 
-model = core.Model.load('nota_50.pth', ['nota_50'])
+model = core.Model.load('notas.pth', ['nota_10','nota20',
+                                      'nota100','25cent','nota_5','1real','nota_2','5cent','maca','nota50','50cent'])
 # image = utils.read_image('frames/dinheiro13.jpg')
 # predictions = model.predict_top(image)
 # labels, boxes, scores = predictions
@@ -18,37 +20,48 @@ while cap.isOpened():
 
     predictions = model.predict_top(frame)
     labels, boxes, scores = predictions
-    detectado = (scores[0] > 0.7)
-    label_detectado = labels[0]
 
-    # Quando detectar um score maio que 0.7, iremos ter o retorno sonoro da detecção
-    #'nota10','nota20','nota100','25cent','nota_5','1real','nota_2','5cent','maca','nota50','50cent'
-    if detectado:
-        print(label_detectado)
-        if label_detectado == "nota_50":
-            engine.say("50 reais")
-        elif label_detectado == "nota20":
-            engine.say("20 reais")
-        elif label_detectado == "25cent":
-            engine.say("25 centavos")
-        elif label_detectado == "nota_5":
-            engine.say("5 reais")
-        elif label_detectado == "1real":
-            engine.say("1 real")
-        elif label_detectado == "nota_2":
-            engine.say("2 reais")
-        elif label_detectado == "5cent":
-            engine.say("5 centavos")
-        elif label_detectado == "maca":
-            engine.say("Maçã")
-        elif label_detectado == "50cent":
-            engine.say("50 centavos")
-        elif label_detectado == "nota_10":
-            engine.say("10 reais")
+    try:
+        scores_numpy = scores.numpy()
+        print(scores_numpy)
+        print(labels)
+        max_value = max(scores_numpy)
 
-        engine.runAndWait()
+        max_index = np.argmax(scores_numpy)
+        detectado = (max_value > 0.5)
+        label_detectado = labels[max_index]
+        # Quando detectar um score maio que 0.7, iremos ter o retorno sonoro da detecção
+        #'nota10','nota20','nota100','25cent','nota_5','1real','nota_2','5cent','maca','nota50','50cent'
+        if detectado:
+            print(label_detectado)
+            if label_detectado == "nota50":
+                engine.say("50 reais")
+            elif label_detectado == "nota20":
+                engine.say("20 reais")
+            elif label_detectado == "25cent":
+                engine.say("25 centavos")
+            elif label_detectado == "nota_5":
+                engine.say("5 reais")
+            elif label_detectado == "1real":
+                engine.say("1 real")
+            elif label_detectado == "nota_2":
+                engine.say("2 reais")
+            elif label_detectado == "5cent":
+                engine.say("5 centavos")
+            elif label_detectado == "maca":
+                engine.say("Maçã")
+            elif label_detectado == "50cent":
+                engine.say("50 centavos")
+            elif label_detectado == "nota_10":
+                engine.say("10 reais")
+            elif label_detectado == "nota100":
+                engine.say("100 reais")
 
-    cv2.imshow('Object detection', frame)
+            engine.runAndWait()
+        cv2.imshow('Object detection', frame)
+
+    except:
+        cv2.imshow('Object detection', frame)
 
     if cv2.waitKey(10) ** 0xFF == ord('q'):
         break
